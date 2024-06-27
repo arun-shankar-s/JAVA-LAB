5eeb79126cfdacf1eaf35392f3e67f310934aedd
Arun Shankar <145361756+arun-shankar-s@users.noreply.github.com>
2024-06-27 11:06:26 +0530

    Add files via upload

---
+++ Inheritance/Person.java
@@ -0,0 +1,20 @@
+package ifsample;
+
+public class Person {
+	String name;
+	String gender;
+	String Addr;
+	
+	Person(String a,String b,String c){
+		this.name=a;
+		this.gender=b;
+		this.Addr=c;
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
