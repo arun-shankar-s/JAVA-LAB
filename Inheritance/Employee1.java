ae8f4b853f5f4a2e4456ddef8ce89a886f358665
Arun Shankar <145361756+arun-shankar-s@users.noreply.github.com>
2024-06-27 11:06:02 +0530

    Add files via upload

---
+++ Inheritance/Employee1.java
@@ -0,0 +1,19 @@
+package ifsample;
+
+public class Employee1 extends Person {
+	
+
+	int eno;
+	double esalary;
+	Employee1(String a, String b, String c,int d,double e) {
+		super(a, b, c);
+		this.eno=d;
+		this.esalary=e;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
