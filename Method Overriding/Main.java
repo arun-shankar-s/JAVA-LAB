b5d7a1381bab7b57e089c38909c6acfeb05a5d36
Arun Shankar <145361756+arun-shankar-s@users.noreply.github.com>
2024-07-16 19:41:10 +0530

    Update Main.java

--- Method Overriding/Main.java
+++ Method Overriding/Main.java
@@ -14,13 +14,22 @@ class Dog extends Animal {
 		System.out.println("I am a dog");
 	}
 }
+class Cat extends Animal {
+
+	public void disp() {
+		super.disp();
+		System.out.println("I am a cat");
+	}
+}
 
 public class Main {
 
 	public static void main(String[] args) {
-		Dog obj=new Dog();
+		Cat obj=new Cat();
 		obj.disp();
 		
+		//Dog obj1=new Dog();
+		//obj.disp();
 	}
 
 }
